c2df0abf4fdea82434c0cd918489353e1631961f
justinmmeans <justin.means@temple.edu>
2024-03-29 16:43:23 -0400

    Added functionality to sort columns

--- dataframe.py
+++ dataframe.py
@@ -8,7 +8,7 @@ def create_dataframe(track_data):
 # Create a DataFrame from the track data
     df = pd.DataFrame(track_data)
     
-    # onvert the Album Artwork URL column to HTML
+    #Convert the Album Artwork URL column to HTML
     df["Art"] = df["Art"].apply(lambda url: f'<img src="{url}" width="50" >')
     
     #Offset the DataFrame index so the top row is 1
@@ -26,9 +26,22 @@ def create_dataframe(track_data):
     </style>
     """
     
+    #Makes the table sortable
+    js = """
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $('.mystyle').DataTable();
+    });
+    </script>
+    """
+    
     #Save the DataFrame to an HTML file
     with open('df.html', 'w') as f:
         f.write(css)
+        f.write(js)
         f.write(df_html)
     
     #Open the HTML file
