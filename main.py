051bc61668a52ed256eb978f39629de15693970e
justinmmeans <justin.means@temple.edu>
2024-04-01 14:22:17 -0400

    Displays songs in top limit with same pitch, need to mod so displays only songs with desired up to the limit requested

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ app = typer.Typer()
 
 
 def uri_from_search(name: str, search_type: str):
-    results = spotify.search(q=f"{search_type}:" + name, type=search_type)
+    #limit determines amount of results, artist and album are limited to 10 results via API
+    results = spotify.search(q=f"{search_type}:" + name, type=search_type, limit=50)
     items = results[search_type + "s"]["items"]
+    #prints the amount of results
+    print(f"Number of results: {len(items)}")
     if len(items) > 0:
         return [item["id"] for item in items]
     else:
@@ -17,25 +20,48 @@ def uri_from_search(name: str, search_type: str):
 
 
 @app.command()
-def top_tracks(artist: str = typer.Option(None, '-a', '--artist'), song: str = typer.Option(None, '-s', '--song')):
-    if song:
-        search_type = "track"
-        name = song
-    elif artist:
+#top_tracks passed arguments based on flags such as -a or -s
+def top_tracks(artist: str = typer.Option(None, '-a', '--artist'),
+               song: str = typer.Option(None, '-s', '--song'),
+               pitch: str = typer.Option(None, '-p', '--pitch')):
+    #artist flag passed limited to 10 results
+    if artist:
         search_type = "artist"
         name = artist
+    #song flag passed limited by limit= in uri_from_search
+    elif song:
+        search_type = "track"
+        name = song
+    #pitch flag passed. Corresponds to 0-11 value
+    elif pitch:
+        search_type = "track"
+        name = pitch
     else:
         raise ValueError("You must provide either an artist (-a) or a song (-s).")
     ids = uri_from_search(name, search_type)
     track_data = []
     if search_type == "track":
         for id in ids:
-            results = spotify.track(id)
-            track_data.append({
-                "Art": results["album"]["images"][0]["url"],
-                "Artist": results["artists"][0]["name"], 
-                "Song": results["name"] 
-            })
+            if pitch is not None:
+                # only pull audio features if pitch if specified
+                pitch_feature = spotify.audio_features(id)[0]
+                if pitch_feature is not None and pitch_feature["key"] is not None and pitch_feature["key"] == int(pitch):
+                    results = spotify.track(id)
+                    track_info = {
+                        "Art": results["album"]["images"][0]["url"],
+                        "Artist": results["artists"][0]["name"], 
+                        "Song": results["name"],
+                        "Pitch": pitch_feature["key"]
+                    }
+                    track_data.append(track_info)
+            else:
+                results = spotify.track(id)
+                track_info = {
+                    "Art": results["album"]["images"][0]["url"],
+                    "Artist": results["artists"][0]["name"], 
+                    "Song": results["name"]
+                }
+                track_data.append(track_info)
     else:
         for id in ids:
             results = spotify.artist_top_tracks(artist_id=id, country="US")
