d2d1d27e8db004e814c5b649dd4cc5f74ad4c74f
tun51789 <tun51789@temple.edu>
2024-04-11 13:56:58 -0400

    added add_to_playlist function, modified get_playlists_info

--- create_playlist.py
+++ create_playlist.py
@@ -3,11 +3,11 @@ from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
 
 class Credentials():
-    def __init__(self, client_id:str = "", client_secret:str = "", username:str = ""):
+    def __init__(self, client_id, client_secret, username):
         self.client_id = client_id
-        self.client_secret=client_secret,
-        self.username=username,
-        self.redirect_uri="http://localhost:5000",
+        self.client_secret = client_secret
+        self.username = username
+        self.redirect_uri = "http://localhost:5000"
         self.scope = "playlist-modify-private playlist-modify-public"
 
 
@@ -26,11 +26,9 @@ def create_playlist(name:str = "test-playlist", is_public:bool = True)->str:
             scope = cred.scope
         )
     )
-    
-    exit()
     return sp.user_playlist_create(user=sp.current_user()["id"], name=name, public=is_public)["id"]
 
-def get_playlist()->pd.DataFrame:
+def get_playlists_info()->pd.DataFrame:
     #Remove later after testing
     client_id = "0e4b89bd47944da690d39e5665b146cc"
     client_secret = "aea56900dd914ebda88809a946993697"
@@ -47,33 +45,50 @@ def get_playlist()->pd.DataFrame:
         )
     )
 
-    results = sp.current_user_top_tracks(limit=10, offset=0,time_range='medium_term')
-
-    track_name = []
-    track_id = []
-    artist = []
-    album = []
-    duration = []
-    popularity = []
-    for i, items in enumerate(results['items']):
-            track_name.append(items['name'])
-            track_id.append(items['id'])
-            artist.append(items["artists"][0]["name"])
-            duration.append(items["duration_ms"])
-            album.append(items["album"]["name"])
-            popularity.append(items["popularity"])
+    results = sp.current_user_playlists(limit=10, offset=0)
+    playlist_name = []
+    playlist_id = []
+    playlist_public = []
+    playlist_tracks = []
+    for item in results["items"]:
+        playlist_name.append(item["name"])
+        playlist_id.append(item["id"])
+        playlist_public.append(item["public"])
+        playlist_tracks.append(item["tracks"])
 
     # Create the final df   
-    df = pd.DataFrame({ "track_name": track_name, 
-                                "album": album, 
-                                "track_id": track_id,
-                                "artist": artist, 
-                                "duration": duration, 
-                                "popularity": popularity})
+    df = pd.DataFrame({ "playlist_name": playlist_name, 
+                                "playlist_id": playlist_id, 
+                                "playlist_public": playlist_public,})
 
     return df
 
+def add_to_playlist(playlist_id:str, song_uri:list, position:int = 0)->int:
+    #Remove later after testing
+    client_id = "0e4b89bd47944da690d39e5665b146cc"
+    client_secret = "aea56900dd914ebda88809a946993697"
+    username = "Gabriel Lopes Carvalho"
+    cred = Credentials(client_id, client_secret, username)
+    
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=cred.client_id,
+            client_secret=cred.client_secret,
+            username=cred.username,
+            redirect_uri=cred.redirect_uri,
+            scope = cred.scope
+        )
+    )
+
+    sp.playlist_add_items(playlist_id=playlist_id, items=song_uri, position=position)
+
+
 
 if __name__ == "__main__":
-    create_playlist()
+    new_playlist = create_playlist("Software Design Shits 2")
+    print(get_playlists_info().head(10))
+    songs = ["spotify:track:3vkQ5DAB1qQMYO4Mr9zJN6",
+             "spotify:track:2245x0g1ft0HC7sf79zbYN",
+             "spotify:track:3ktTWpYdXrYApH54cBo4Ap"]
+    
+    add_to_playlist(new_playlist, songs)
     
