7c6a18b70700dc5d8f46ba46a65051092fa3abfc
justinmmeans <justin.means@temple.edu>
2024-04-13 14:47:17 -0400

    Reduced amount of API calls, filtered out some info from dataframe

--- user_tracks.py
+++ user_tracks.py
@@ -10,32 +10,40 @@ def generate_user_tracks(limit=50):
 
     #Retrieve the user's saved tracks
     saved_tracks = user_spotify.current_user_saved_tracks()
-    saved_track_ids = [track['track']['id'] for track in saved_tracks['items']]
-    
-    #Retrieves up to limit amount of users top songs over a period of time
-    top_tracks = user_spotify.current_user_top_tracks(time_range='long_term', limit=25)
-    top_track_ids = [track['id'] for track in top_tracks['items']]
-    
-    top_artist_genres = [user_spotify.artist(track['artists'][0]['id'])['genres'] for track in top_tracks['items']]
+    saved_track_ids = set(track['track']['id'] for track in saved_tracks['items'])
 
-    #The value amounts for most_common_genres + seeded_tracks must equal 5
+    #Retrieve the user's top tracks up to limit
+    top_tracks = user_spotify.current_user_top_tracks(time_range='long_term', limit=25)
+
+    #Get the artist IDs from the top tracks
+    artist_ids = [track['artists'][0]['id'] for track in top_tracks['items']]
+
+    #Get information about all user saved artists
+    artists = user_spotify.artists(artist_ids)
+
+    #Get the genres of their artists
+    top_artist_genres = [artist['genres'] for artist in artists['artists']]
+
+    #Get the song IDs
+    top_track_ids = [track['id'] for track in top_tracks['items']]
+
+    #Find the most common genres. The amount for this + seeded tracks must = 5
     flattened_genres = [genre for genres in top_artist_genres for genre in genres]
-    most_common_genres = [genre for genre, _ in collections.Counter(flattened_genres).most_common(1)]
-    
-    #Combine seeding for song_recommendations
-    #The value amounts for most_common_genres + seeded_tracks must equal 5
-    seeded_tracks = (saved_track_ids[:3] + top_track_ids[:1])
-    
-    #Retrieve song recommendations based on seeds
-    song_recommendations = user_spotify.recommendations(seed_tracks=seeded_tracks, seed_genres=most_common_genres, limit=limit)
+    most_common_genre = collections.Counter(flattened_genres).most_common(1)[0][0]
+
+    #Combine seeds for song recommendations
+    seeded_tracks = (list(saved_track_ids)[:3] + top_track_ids[:1])
+
+    #Retrieve song recommendations based on track and genre seeds
+    song_recommendations = user_spotify.recommendations(seed_tracks=seeded_tracks, seed_genres=[most_common_genre], limit=limit)
 
     #Prepare the data for create_dataframe
     track_data = []
     for track in song_recommendations['tracks']:
-        #skip the song if already saved
+        #Skip the song if already saved
         if track['id'] in saved_track_ids:
             continue
-        #else add it to be output to dataframe
+        #Else add it to be output to dataframe
         track_info = {
             "Art": track["album"]["images"][0]["url"],
             "Artist": track["artists"][0]["name"],
@@ -43,10 +51,11 @@ def generate_user_tracks(limit=50):
         }
         track_audio_features = user_spotify.audio_features(track["id"])[0]
         for audio_feature in track_audio_features:
-            track_info[audio_feature] = track_audio_features[audio_feature]
+            if audio_feature not in ['type', 'id', 'uri', 'track_href', 'analysis_url']:
+                track_info[audio_feature] = track_audio_features[audio_feature]
         track_data.append(track_info)
 
-    #Create a dataframe from the song recommendations
+    #sCreate a dataframe from the song recommendations
     df = create_dataframe(track_data)
 
     return df
