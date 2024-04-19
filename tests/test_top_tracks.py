2713a66dce3b64d76c1574bdd7882986e11b0c17
dennis-yeom <tuf64483@temple.edu>
2024-04-18 20:34:56 -0400

    added speechiness initial value

--- tests/test_top_tracks.py
+++ tests/test_top_tracks.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def test_get_top_tracks_artist():
     #Checks top tracks for The Wonder Years
-    df = get_top_tracks(artist='The Wonder Years', song=None, pitch=None, tempo=None, danceability=None, time_signature=None, acousticness=None, liveness=None, energy=None, help=None, save=None, load=None)
+    df = get_top_tracks(artist='The Wonder Years', song=None, pitch=None, tempo=None, danceability=None, time_signature=None, acousticness=None, liveness=None, energy=None, speechiness=None, help=None, save=None, load=None)
     #Check df has results
     assert not df.empty
     #Check output artists match input
@@ -12,7 +12,7 @@ def test_get_top_tracks_artist():
 
 def test_get_top_tracks_song():
     #Checks top tracks for Came Out Swinging
-    df = get_top_tracks(song='Came Out Swinging', artist=None, pitch=None, tempo=None, danceability=None, time_signature=None, acousticness=None, liveness=None, energy=None, help=None, save=None, load=None)
+    df = get_top_tracks(song='Came Out Swinging', artist=None, pitch=None, tempo=None, danceability=None, time_signature=None, acousticness=None, liveness=None, energy=None, speechiness=None, help=None, save=None, load=None)
     #Check df has results
     assert not df.empty
     #Check that output song names contains the input song
