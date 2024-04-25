dd05c64f1238092331b5826bdc474210fdd0d91a
tuo20482 <tuo20482@temple.edu>
2024-04-25 19:47:33 -0400

    Fixed top_tracks test

--- tests/test_top_tracks.py
+++ tests/test_top_tracks.py
@@ -3,5 +3,23 @@ from main import top_tracks
 
 def test_top_tracks():
     #Checks top tracks for The Wonder Years
-    df = top_tracks('The Wonder Years')
+    df = top_tracks('The Wonder Years',
+                    save=False,
+                    load=False,
+                    acousticness=None,
+                    danceability=None,
+                    duration_ms=None,
+                    energy=None,
+                    instrumentalness=None,
+                    key=None,
+                    liveness=None,
+                    loudness=None,
+                    mode=None,
+                    popularity=None,
+                    speechiness=None,
+                    tempo=None,
+                    time_signature=None,
+                    valence=None)
+    assert len(df) == 10
+    assert "The Wonder Years" in df["Artists"].values
 
